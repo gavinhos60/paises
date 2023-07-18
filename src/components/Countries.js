e9175450cf8b7ab3a8738d4c38b0c4f11cf58cfd
Rafael <gavinhos60@gmail.com>
2023-07-18 18:01:55 -0300

    bandeiras e dark mode

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { Search } from "lucide-react";
 
+
+
 import "../style.css/countries.css";
 
 const url = "https://restcountries.com/v3.1/all";
 
+ 
+
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+
+ 
 
   const fetchCountryData = async () => {
     const response = await fetch(url);
@@ -19,40 +25,254 @@ const Countries = () => {
   }, []);
 
   const germany = countries.filter(
-    (country) => country.name.common === "Germany"
+    (country) => country.name.common === "Spain"
   );
-
+  const eua = countries.filter(
+    (country) => country.name.common === "Suriname"
+  );
+  const brasil = countries.filter(
+    (country) => country.name.common === "Brazil"
+  );
+  const iceland = countries.filter(
+    (country) => country.name.common === "Iceland"
+  );
+  const afg = countries.filter(
+    (country) => country.name.common === "Afghanistan"
+  );
+  const frace = countries.filter(
+    (country) => country.name.common === "France"
+  );
+  const italia = countries.filter(
+    (country) => country.name.common === "Italy"
+  );
+  const portugal = countries.filter(
+    (country) => country.name.common === "Portugal"
+  );
   return (
-    <div className="flex items-center justify-center mt-16">
-      <div className="w-5/6 h-800 flex justify-between items-start  ">
-        <div>
-          <div className=" bg-header rounded text-center flex items-center shadow-md w-450 h-12 ">
-            <button>
-              <Search color="white" size={26} className=" ml-6 mr-2 mb-1" />
-            </button>
-            <input
-              type="text"
-              className="bg-header w-full h-full ml-6  flex text-center text-white"
-              placeholder="Search for a contry..."
-            />
+    
+    <div className="">      
+      <div className="flex items-center justify-center mt-16 ">
+        <div className="w-5/6 flex justify-between items-start  ">
+          <div>
+            <div className=" bg-header rounded text-center flex items-center shadow-md w-450 h-12">
+              <button>
+                <Search color="white" size={26} className=" ml-6 mr-2 mb-1" />
+              </button>
+              <input
+                type="text"
+                className="bg-header w-full h-full ml-6  flex text-center text-white"
+                placeholder="Search for a contry..."
+              />
+            </div>
+          </div>
+          <div className="">
+            <select className="bg-header h-12 ml-6 text-center w-44 text-white rounded">
+              <option value="valor1">None</option>
+              <option value="valor2">Valor 2</option>
+              <option value="valor3">Valor 3</option>
+            </select>
           </div>
         </div>
-        <div className="">
-          <select className="bg-header h-12 ml-6 text-center w-44 text-white rounded">
-            <option value="valor1">None</option>
-            <option value="valor2">Valor 2</option>
-            <option value="valor3">Valor 3</option>
-          </select>
-        </div>
       </div>
-      {/*} 
-      
-    {germany.map((country) => (
+      <div className="w-5/6 h-750 ml-40 ">
+        <div class="grid grid-cols-4 gap-36 mt-10">
+          <div className="dark-mode">
+        <div class="bg-header w-294  text-white rounded shadow-2xl">
+            {germany.map((country) => (
+              <article key={country.name.common}>
+                <div className="">
+                  <img
+                    src={country.flags.svg}
+                    alt={`Bandeira ${country.name.common}`}
+                    className="w-full h-auto block "
+                  />
+                </div>
+                <div className="p-5 font-bold">
+                  <h3>{country.name.common}</h3>
+                  <div className="font-normal">
+                    <div className="mt-3 text-xs">
+                      Population: {country.population}
+                    </div>
+                    <p className="mt-1 text-xs">Region: {country.region}</p>
+                    <p className="mt-1 text-xs">Capital: {country.capital}</p>
+                  </div>
+                </div>
+              </article>
+            ))}
+          </div>
+          </div>
+          <div class="bg-header w-294  text-white rounded shadow-2xl">
+            {eua.map((country) => (
+              <article key={country.name.common}>
+                <div className="">
+                  <img
+                    src={country.flags.svg}
+                    alt={`Bandeira ${country.name.common}`}
+                    className="w-full h-auto block "
+                  />
+                </div>
+                <div className="p-5 font-bold">
+                  <h3>{country.name.common}</h3>
+                  <div className="font-normal">
+                    <div className="mt-3 text-xs">
+                      Population: {country.population}
+                    </div>
+                    <p className="mt-1 text-xs">Region: {country.region}</p>
+                    <p className="mt-1 text-xs">Capital: {country.capital}</p>
+                  </div>
+                </div>
+              </article>
+            ))}
+          </div>
+          <div class="bg-header w-294  text-white rounded shadow-2xl">
+            {brasil.map((country) => (
+              <article key={country.name.common}>
+                <div className="">
+                  <img
+                    src={country.flags.svg}
+                    alt={`Bandeira ${country.name.common}`}
+                    className="w-full h-auto block "
+                  />
+                </div>
+                <div className="p-5 font-bold">
+                  <h3>{country.name.common}</h3>
+                  <div className="font-normal">
+                    <div className="mt-3 text-xs">
+                      Population: {country.population}
+                    </div>
+                    <p className="mt-1 text-xs">Region: {country.region}</p>
+                    <p className="mt-1 text-xs">Capital: {country.capital}</p>
+                  </div>
+                </div>
+              </article>
+            ))}
+          </div>
+          <div class="bg-header w-294 text-white rounded shadow-2xl">
+            {iceland.map((country) => (
+              <article key={country.name.common}>
+                <div className="">
+                  <img
+                    src={country.flags.svg}
+                    alt={`Bandeira ${country.name.common}`}
+                    className="w-full h-52 block"
+                  />
+                </div>
+                <div className="p-5 font-bold">
+                  <h3>{country.name.common}</h3>
+                  <div className="font-normal">
+                    <div className="mt-3 text-xs">
+                      Population: {country.population}
+                    </div>
+                    <p className="mt-1 text-xs">Region: {country.region}</p>
+                    <p className="mt-1 text-xs">Capital: {country.capital}</p>
+                  </div>
+                </div>
+              </article>
+            ))}
+          </div>
+        </div>
+        <div class="grid grid-cols-4 gap-36 mt-20">
+          <div class="bg-header w-294 text-white rounded shadow-2xl">
+            {afg.map((country) => (
+              <article key={country.name.common}>
+                <div className="">
+                  <img
+                    src={country.flags.svg}
+                    alt={`Bandeira ${country.name.common}`}
+                    className="w-full h-48 block"
+                  />
+                </div>
+                <div className="p-5 font-bold">
+                  <h3>{country.name.common}</h3>
+                  <div className="font-normal">
+                    <div className="mt-3 text-xs">
+                      Population: {country.population}
+                    </div>
+                    <p className="mt-1 text-xs">Region: {country.region}</p>
+                    <p className="mt-1 text-xs">Capital: {country.capital}</p>
+                  </div>
+                </div>
+              </article>
+            ))}
+          </div>
+          <div class="bg-header w-294 text-white rounded shadow-2xl">
+            {frace.map((country) => (
+              <article key={country.name.common}>
+                <div className="">
+                  <img
+                    src={country.flags.svg}
+                    alt={`Bandeira ${country.name.common}`}
+                    className="w-full h-auto block"
+                  />
+                </div>
+                <div className="p-5 font-bold">
+                  <h3>{country.name.common}</h3>
+                  <div className="font-normal">
+                    <div className="mt-3 text-xs">
+                      Population: {country.population}
+                    </div>
+                    <p className="mt-1 text-xs">Region: {country.region}</p>
+                    <p className="mt-1 text-xs">Capital: {country.capital}</p>
+                  </div>
+                </div>
+              </article>
+            ))}
+          </div>
+          <div class="bg-header w-294 text-white rounded shadow-2xl">
+            {italia.map((country) => (
+              <article key={country.name.common}>
+                <div className="">
+                  <img
+                    src={country.flags.svg}
+                    alt={`Bandeira ${country.name.common}`}
+                    className="w-full h-auto block"
+                  />
+                </div>
+                <div className="p-5 font-bold">
+                  <h3>{country.name.common}</h3>
+                  <div className="font-normal">
+                    <div className="mt-3 text-xs">
+                      Population: {country.population}
+                    </div>
+                    <p className="mt-1 text-xs">Region: {country.region}</p>
+                    <p className="mt-1 text-xs">Capital: {country.capital}</p>
+                  </div>
+                </div>
+              </article>
+            ))}
+          </div>
+          <div class="bg-header w-294 text-white rounded shadow-2xl">
+            {portugal.map((country) => (
+              <article key={country.name.common}>
+                <div className="">
+                  <img
+                    src={country.flags.svg}
+                    alt={`Bandeira ${country.name.common}`}
+                    className="w-full h-auto block"
+                  />
+                </div>
+                <div className="p-5 font-bold">
+                  <h3>{country.name.common}</h3>
+                  <div className="font-normal">
+                    <div className="mt-3 text-xs">
+                      Population: {country.population}
+                    </div>
+                    <p className="mt-1 text-xs">Region: {country.region}</p>
+                    <p className="mt-1 text-xs">Capital: {country.capital}</p>
+                  </div>
+                </div>
+              </article>
+            ))}
+          </div>
+        </div>
+        
+        {/*{germany.map((country) => (
         <article key={country.name.common}>
           <h3>{country.name.common}</h3>
           <img src={country.flags.svg} alt={`Bandeira ${country.name.common}`} />
     </article>
-      ))}     */}
+      ))} >*/}
+      </div>
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,57 @@
-import React from "react";
-import { Moon } from 'lucide-react';
+import React, { useState, useEffect } from "react";
+import { Moon } from "lucide-react";
+import classnames from "classnames";
 
 const Header = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  useEffect(() => {
+    const body = document.querySelector("body");
+    if (darkMode) {
+      body.classList.add("dark");
+    } else {
+      body.classList.remove("dark");
+    }
+  }, [darkMode]);
+
+  useEffect(() => {
+    const header = document.querySelector("header");
+    if (darkMode) {
+      header.classList.add("dark");
+    } else {
+      header.classList.remove("dark");
+    }
+  }, [darkMode]);
+  
+
+  const buttonClasses = classnames(
+    "text-black border-none  w-48 mr-20 flex items-center text-1xl",
+    {
+      "text-black": darkMode,
+    }
+  );
+
   return (
-    <header class="w-full h-20 text-white bg-header flex items-center font-bold ">
-      <h1 class="text-2xl ml-12 ">Where in the world?</h1>
-      <div class="ml-auto "> 
-             
-        <button class="text-white border-nonew-48 mr-20 flex items-center text-1xl "><Moon color="black" fill="#FFFFFF"  size={26} stroke="#FFFFFF" className="mr-2 mb-1"/>Dark Mode</button>
+    <header className="w-full h-20 text-white bg-header flex items-center font-bold">
+      <h1 className="text-2xl ml-12">Where in the world?</h1>
+      <div className="ml-auto">
+        <button className={buttonClasses} onClick={toggleDarkMode}>
+          <Moon
+            color={darkMode ? "black" : "black"}
+            fill={darkMode ? "black" : "black"}
+            size={26}
+            stroke={darkMode ? "black" : "black"}
+            className="mr-2 mb-1"
+          />
+          <span>{darkMode ? "Light Mode" : "Dark Mode"}</span>
+        </button>
       </div>
     </header>
   );
-  
 };
 
 export default Header;
